d8e7d185c6a3df98d7574cbdb4876033e8319fc4
poojanilangekar <nilangekar.pooja@gmail.com>
2017-12-08 17:37:16 -0500

    Removed binaries

--- libcuckoo/build/tests/Catch/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/poojanilangekar/Documents/ACF/libcuckoo/tests/Catch
-# Build directory: /Users/poojanilangekar/Documents/ACF/libcuckoo/build/tests/Catch
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- libcuckoo/build/tests/unit-tests/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/poojanilangekar/Documents/ACF/libcuckoo/tests/unit-tests
-# Build directory: /Users/poojanilangekar/Documents/ACF/libcuckoo/build/tests/unit-tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(unit_tests "/Users/poojanilangekar/Documents/ACF/libcuckoo/build/tests/unit-tests/unit_tests")
